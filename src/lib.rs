2c49375c91a0ec4714d2b2fd7ff429de4b8a6057
Dongdong Zhou <dzhou121@gmail.com>
2023-03-30 20:14:32 +0100

    add scissor

--- src/lib.rs
+++ src/lib.rs
@@ -695,6 +695,7 @@ impl Vger {
                 (rect.y + rect.height) as f32,
             ];
             prim.paint = paint_index.index as u32;
+            prim.scissor = self.add_scissor() as u32;
 
             self.render(prim);
         }
@@ -732,6 +733,7 @@ impl Vger {
             if let Some(paint_index) = paint_index {
                 prim.paint = paint_index.index as u32;
             }
+            prim.scissor = self.add_scissor() as u32;
 
             self.render(prim);
         }
